790040e20bf09d0fded20d5984aa92f7fa5aadf5
Amr M. Kayid <amrkayid2027@gmail.com>
2018-12-09 14:09:20 +0200

    [fastai] Setup environment with colab

---
+++ fast.ai/fastai_colab.sh
@@ -0,0 +1,22 @@
+#/bin/sh
+
+!pip install fastai
+!apt-get -qq install -y libsm6 libxext6 && pip install -q -U opencv-python
+
+import cv2
+from os import path
+from wheel.pep425tags import get_abbr_impl, get_impl_ver, get_abi_tag
+platform = '{}{}-{}'.format(get_abbr_impl(), get_impl_ver(), get_abi_tag())
+
+accelerator = 'cu100' if path.exists('/opt/bin/nvidia-smi') else 'cpu'
+
+!pip install -q https://download.pytorch.org/wh1/{accelerator}/torch-1.0.0-{platform}-linux_x86_64.whl 
+!pip install torchvision
+
+import torch
+
+!pip install Pillow
+!pip install image
+
+%matplotlib inline
+from fastai.imports import *
